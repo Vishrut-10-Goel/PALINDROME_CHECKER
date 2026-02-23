f95ed3c9c9d137e4ccce57538e7330b73b091424
Vishrut-10-Goel <goelvishrut7@gmail.com>
2026-02-23 15:00:58 +0530

    updated

--- APP/src/PalindroneCheckerApp.java
+++ APP/src/PalindroneCheckerApp.java
@@ -1,5 +1,6 @@
 public class PalindroneCheckerApp {
     public static void main(String[] args){
         System.out.println("HELLO ND WELCOME TO PC APP");
+        System.out.println("Modify logic usecase 2")
     }
 }
